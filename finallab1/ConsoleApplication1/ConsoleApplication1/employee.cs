b6753262ddc972fb4d04adc20da5cd53b126db44
hamnaamanullah4 <95175159+hamnaamanullah4@users.noreply.github.com>
2021-11-30 13:19:49 +0500

    Add files via upload

---
+++ finallab1/ConsoleApplication1/ConsoleApplication1/employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class employee : hourlyPaid
+    {
+        public char employeeID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string employeename
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int employeetype
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int monthlyhoursworked
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int unitproduced
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void calculate_salary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ finallab1/ConsoleApplication1/ConsoleApplication1/hourlyEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class hourlyPaid : employees
+    {
+        private int base_hour;
+        private int hourlypayrate;
+        private int no_of_units;
+        private int overtime_rate;
+
+        public void calculate_salary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
